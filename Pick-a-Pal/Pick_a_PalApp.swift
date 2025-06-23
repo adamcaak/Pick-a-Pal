8cf04ca3d47f97a780c5033c1124d361dea222fc
adamcaak <ladamczak1997@gmail.com>
2025-06-24 00:33:18 +0200

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Łukasz Adamczak on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
